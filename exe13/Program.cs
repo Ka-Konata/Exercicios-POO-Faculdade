cb384af193d68dbd0a9aad6cefbc4020d738e89c
Victor G. Ramos <69207529+Ka-Konata@users.noreply.github.com>
2023-04-15 23:57:54 -0300

    .

---
+++ exe13/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Exe1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lampada lampada = new Lampada(true);
+            lampada.Imprimir();
+            lampada.Desligar();
+            lampada.Imprimir();
+            lampada.Ligar();
+            lampada.Imprimir();
+        }
+    }
+}
